ae9d3610aacc5ea5c965c13b9fb7533d4a43d53c
lucasezidro <lucasezidro7@gmail.com>
2024-04-20 16:05:08 -0300

    create layout for success page

--- src/pages/success.tsx
+++ src/pages/success.tsx
@@ -1,4 +1,20 @@
+import Link from "next/link";
+import { ImageContainer, SuccessContainer } from "../styles/pages/success";
+
 export default function Product () {
 
-  return <h1>Success</h1>
+  return (
+    <SuccessContainer>
+      <h1>Compra Efetuada!</h1>
+      <ImageContainer>
+
+      </ImageContainer>
+
+      <p>Uhuul <strong>Lucas Ezidro</strong>, sua <strong>Camiseta Beyond the Limits</strong> já está a caminho da sua casa. </p>
+
+      <Link href="/">
+        Voltar ao catálogo
+      </Link>
+    </SuccessContainer>
+  )
 }
