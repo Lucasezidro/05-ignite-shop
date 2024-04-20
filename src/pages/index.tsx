80797715836a28714a09247f5708f4631f913db3
lucasezidro <lucasezidro7@gmail.com>
2024-04-20 14:17:48 -0300

    config getStaticPaths for dynamic props

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,7 +13,7 @@ interface HomeProps {
     id: string
     name: string
     imageUrl: string
-    price: number
+    price: string
   }[]
 }
 
@@ -72,6 +72,6 @@ export const getStaticProps: GetStaticProps = async () => {
     props: {
       products
     },
-    revalidate: 60 * 60 * 2, // 2 horas
+    revalidate: 60 * 60 * 2, // 2 hours
   }
 }
