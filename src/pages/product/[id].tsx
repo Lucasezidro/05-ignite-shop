446a2e0565d876d86b1534eb50bd5b983c006d6d
lucasezidro <lucasezidro7@gmail.com>
2024-04-20 13:15:57 -0300

    create styles for product page

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,8 +1,24 @@
 import { useRouter } from "next/router"
+import { ImageContainer, ProductContainer, ProductDetails } from "../../styles/pages/product"
+import Image from "next/image"
 
 export default function Product () {
   const { query } = useRouter()
 
 
-  return <h1>Product: {JSON.stringify(query)}</h1>
+  return (
+    <ProductContainer>
+      <ImageContainer>
+      </ImageContainer>
+
+      <ProductDetails>
+        <h1>Camiseta X</h1>
+
+        <span>R$ 79, 90</span>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur molestias, sint dicta error reprehenderit id eos maxime? Non, aut eaque! Non, sequi est quos dolorem praesentium ullam sunt eius voluptatum.</p>
+
+        <button>Comprar agora</button>
+      </ProductDetails>
+    </ProductContainer>
+  )
 }
